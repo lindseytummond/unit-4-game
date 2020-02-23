4111afb4e7969ae20fb4c32153531c03df3ca9a3
lindsey tummond <lindseytummond@gmail.com>
2020-02-22 22:13:29 -0700

    array for random number and crystal numbers

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    //randomNumber array
+    var randomNumber = [];
+
+    for (var r= 19; r < 121; r++){
+        randomNumber.push (r);
+    }
+
+    //crystalNumber array
+
+    var crystalNumber = [];
+
+    for (var c= 1; c < 12; c++){
+        crystalNumber.push (c);
+    }
+
+
+
+
+
+
+
+
+
+})
+
